80ea190c93270335e6f022ed84d4c8e6960ce63a
eguonoefe <eguonoghene.efekemo@gmail.com>
2018-07-10 13:31:35 +0100

    Create User Routes

---
+++ controllers/User.js
@@ -0,0 +1,27 @@
+class User {
+  create(req, res) {
+    res.send('working');
+  }
+
+  update(req, res) {
+    res.send('working');
+  }
+
+  listUsersById(req, res) {
+    res.send('working');
+  }
+
+  listUsersByMaritalStatus(req, res) {
+    res.send('working');
+  }
+
+  details(req, res) {
+    res.send('working');
+  }
+
+  delete(req, res) {
+    res.send('working');
+  }
+}
+
+export default new User();
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 const port = 8080 || ENV['PORT'];
 
-app.get('/', (req, res) => {
-  res.send('working');
-});
+routes(app);
 
 app.listen(port, () => {
   console.log('Running on port', port);
